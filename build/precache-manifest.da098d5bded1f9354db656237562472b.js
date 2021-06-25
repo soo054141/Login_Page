1623133a14ab35cd868cf56389b85bb478aa2f0a
soo054141 <48131318+soo054141@users.noreply.github.com>
2021-06-26 01:39:58 +0900

    1. build test

---
+++ build/precache-manifest.da098d5bded1f9354db656237562472b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf48c9e2ef4630bbf608e91bf65fb0c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7319744a6eb8125b367d",
+    "url": "/static/js/2.e0192410.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e0192410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ebc31a40d16ea45a181",
+    "url": "/static/js/main.ddb7ab4f.chunk.js"
+  },
+  {
+    "revision": "e72ff7509a54d5946683",
+    "url": "/static/js/runtime-main.372f1ef1.js"
+  }
+]);
